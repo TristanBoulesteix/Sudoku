9554b33e8bdac0e515f06cce210eed9d5ed6a298
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-08-14 18:06:12 +0200

    Initial commit

---
+++ app/src/main/java/com/tboul/sudoku/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.tboul.sudoku
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_main)
+    }
+}
