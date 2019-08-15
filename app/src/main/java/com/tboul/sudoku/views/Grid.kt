5e60a6d3f9d6c85b30283c674a709d0e84ac8fbf
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-08-15 18:57:26 +0200

    Grid created

--- app/src/main/java/com/tboul/sudoku/views/Grid.kt
+++ app/src/main/java/com/tboul/sudoku/views/Grid.kt
@@ -3,113 +3,75 @@ package com.tboul.sudoku.views
 import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
-import android.graphics.drawable.ColorDrawable
-import android.graphics.drawable.Drawable
-import android.util.AttributeSet
-import android.util.TypedValue
-import android.view.View
-import android.app.Activity
 import android.graphics.Paint
-import android.util.DisplayMetrics
-import android.os.Build
-import android.support.annotation.Nullable
-import android.support.annotation.RequiresApi
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
 
+class Grid(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
+    val numColumns: Int = 9
+    val numRows: Int = 9
+    private var cellWidth: Int = 0
+    private var cellHeight: Int = 0
+    private val blackPaint = Paint()
+    private var cellChecked: Array<BooleanArray>? = null
 
-
-class Grid @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
-    //number of row and column
-    private var screenWidth: Int = 0
-    private var screenHeight: Int = 0
-    val GAP_WIDTH_DP = 62
-    private var GAP_WIDTH_PIXEL: Float = 0.toFloat()
-    private val paint = Paint()
-
-    fun PixelGridView(context: Context): ??? {
-        super(context)
-        init(context)
+    init {
+        blackPaint.style = Paint.Style.FILL_AND_STROKE
+        calculateDimensions()
     }
 
-    fun PixelGridView(context: Context,  attrs: AttributeSet): ??? {
-        super(context, attrs)
-        init(context)
-
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        calculateDimensions()
     }
 
-    fun PixelGridView(context: Context, @Nullable attrs: AttributeSet, defStyleAttr: Int): ??? {
-        super(context, attrs, defStyleAttr)
-        init(context)
+    private fun calculateDimensions() {
+        cellWidth = width / this.numColumns
+        cellHeight = height / this.numRows
 
+        cellChecked = Array(this.numColumns) { BooleanArray(this.numRows) }
+
+        invalidate()
     }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    fun PixelGridView(context: Context, @Nullable attrs: AttributeSet, defStyleAttr: Int, defStyleRes: Int): ??? {
-        super(context, attrs, defStyleAttr, defStyleRes)
-        init(context)
-    }
-
-    fun convertDpToPixel(dp: Float, context: Context): Float {
-        return dp * (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
-    }
-
-    fun init(context: Context) {
-
-        // set paint color
-        paint.setColor(Color.GREEN)
-
-        // get view dimentions
-        getScreenDimensions()
-
-    }
-
-    private fun getScreenDimensions() {
-        val displayMetrics = DisplayMetrics()
-        (context as Activity).windowManager.defaultDisplay.getMetrics(displayMetrics)
-        screenWidth = displayMetrics.widthPixels
-        screenHeight = displayMetrics.heightPixels
-
-        //gap size in pixel
-        GAP_WIDTH_PIXEL = convertDpToPixel(GAP_WIDTH_DP.toFloat(), context)
-
-    }
-
-    override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
-        super.onLayout(changed, left, top, right, bottom)
-        getScreenDimensions()
-
-    }
-
 
     override fun onDraw(canvas: Canvas) {
-        super.onDraw(canvas)
+        canvas.drawColor(Color.WHITE)
 
-        // draw Horizontal line from Y= 0 -> Y+=Gap... till screen width
-        var verticalPosition = 0f
+        val width = width
+        val height = height
 
-        while (verticalPosition <= screenHeight) {
+        for (i in 0 until this.numColumns) {
+            for (j in 0 until this.numRows) {
+                if (cellChecked!![i][j]) {
 
-            canvas.drawLine(
-                0f, verticalPosition,
-                screenWidth.toFloat(), verticalPosition, paint
-            )
-
-            verticalPosition += GAP_WIDTH_PIXEL
-
+                    canvas.drawRect(
+                        (i * cellWidth).toFloat(), (j * cellHeight).toFloat(),
+                        ((i + 1) * cellWidth).toFloat(), ((j + 1) * cellHeight).toFloat(),
+                        blackPaint
+                    )
+                }
+            }
         }
 
-        // draw Vertical line from X=0 -> X+=Gap... till screen Height 0|||hor+gap|||W
-        var horizontalPosition = 0f
-
-        while (horizontalPosition <= screenWidth) {
-
-            canvas.drawLine(
-                horizontalPosition, 0f,
-                horizontalPosition, screenHeight.toFloat(), paint
-            )
-
-            horizontalPosition += GAP_WIDTH_PIXEL
-
+        for (i in 1 until this.numColumns) {
+            canvas.drawLine((i * cellWidth).toFloat(), 0f, (i * cellWidth).toFloat(), height.toFloat(), blackPaint)
         }
 
+        for (i in 1 until this.numRows) {
+            canvas.drawLine(0f, (i * cellHeight).toFloat(), width.toFloat(), (i * cellHeight).toFloat(), blackPaint)
+        }
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val column = (event.x / cellWidth).toInt()
+            val row = (event.y / cellHeight).toInt()
+
+            cellChecked!![column][row] = !cellChecked!![column][row]
+            invalidate()
+        }
+
+        return true
     }
 }
