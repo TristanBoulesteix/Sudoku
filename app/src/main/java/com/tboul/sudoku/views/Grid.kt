2515bae4aee325610078bf7a6fb1b1b3b1e908f4
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-08-17 15:14:34 +0200

    Grid done

--- app/src/main/java/com/tboul/sudoku/views/Grid.kt
+++ app/src/main/java/com/tboul/sudoku/views/Grid.kt
@@ -5,33 +5,31 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
-import android.view.MotionEvent
 import android.view.View
 
 class Grid(context: Context, attrs: AttributeSet? = null) : View(context, attrs) {
     private val numColumns: Int = 9
-    private val numRows: Int = 9
+    private val blackPaint = Paint().apply { style = Paint.Style.FILL_AND_STROKE }
+    private val boldPaint = Paint().apply {
+        style = Paint.Style.FILL_AND_STROKE
+        strokeWidth = 5F
+    }
 
     private var cellWidth: Int = 0
     private var cellHeight: Int = 0
-    private val blackPaint = Paint()
-    private var cellChecked: Array<BooleanArray>? = null
 
     init {
-        blackPaint.style = Paint.Style.FILL_AND_STROKE
         calculateDimensions()
     }
 
-    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
-        super.onSizeChanged(w, h, oldw, oldh)
+    override fun onSizeChanged(width: Int, height: Int, oldwidth: Int, oldheight: Int) {
+        super.onSizeChanged(width, height, oldwidth, oldheight)
         calculateDimensions()
     }
 
     private fun calculateDimensions() {
         cellWidth = width / this.numColumns
-        cellHeight = height / this.numRows
-
-        cellChecked = Array(this.numColumns) { BooleanArray(this.numRows) }
+        cellHeight = height / this.numColumns
 
         invalidate()
     }
@@ -42,37 +40,20 @@ class Grid(context: Context, attrs: AttributeSet? = null) : View(context, attrs)
         val width = width
         val height = height
 
-        for (i in 0 until this.numColumns) {
-            for (j in 0 until this.numRows) {
-                if (cellChecked!![i][j]) {
+        canvas.drawLine(width.toFloat(), 0f, width.toFloat(), height.toFloat(), boldPaint)
+        canvas.drawLine(0f, height.toFloat(), width.toFloat(), height.toFloat(), boldPaint)
 
-                    canvas.drawRect(
-                        (i * cellWidth).toFloat(), (j * cellHeight).toFloat(),
-                        ((i + 1) * cellWidth).toFloat(), ((j + 1) * cellHeight).toFloat(),
-                        blackPaint
-                    )
-                }
-            }
-        }
+        canvas.drawLine(0f, 0f, width.toFloat(), 0F, boldPaint)
+        canvas.drawLine(0f, 0F, 0F, height.toFloat(), boldPaint)
 
         for (i in 1 until this.numColumns) {
-            canvas.drawLine((i * cellWidth).toFloat(), 0f, (i * cellWidth).toFloat(), height.toFloat(), blackPaint)
+            val paint = if (i % 3 == 0) {
+                boldPaint
+            } else {
+                blackPaint
+            }
+            canvas.drawLine((i * cellWidth).toFloat(), 0f, (i * cellWidth).toFloat(), height.toFloat(), paint)
+            canvas.drawLine(0f, (i * cellHeight).toFloat(), width.toFloat(), (i * cellHeight).toFloat(), paint)
         }
-
-        for (i in 1 until this.numRows) {
-            canvas.drawLine(0f, (i * cellHeight).toFloat(), width.toFloat(), (i * cellHeight).toFloat(), blackPaint)
-        }
-    }
-
-    override fun onTouchEvent(event: MotionEvent): Boolean {
-        if (event.action == MotionEvent.ACTION_DOWN) {
-            val column = (event.x / cellWidth).toInt()
-            val row = (event.y / cellHeight).toInt()
-
-            cellChecked!![column][row] = !cellChecked!![column][row]
-            invalidate()
-        }
-
-        return true
     }
 }
