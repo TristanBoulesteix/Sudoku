ce3dac401f1ac2228ee30e4610ca5e9171036edd
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-08-23 19:31:02 +0200

    Add responsive design to main activity and creation of fab

--- app/src/main/java/com/tboul/sudoku/views/activities/MainActivity.kt
+++ app/src/main/java/com/tboul/sudoku/views/activities/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.view.Window
 import android.widget.Button
 import com.tboul.sudoku.R
