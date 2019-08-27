3b3f0e43f5b175e98cfcd5392081d98819b43c52
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-08-27 16:35:30 +0200

    Add numbers

--- app/src/main/kotlin/com/tboul/sudoku/views/GridView.kt
+++ app/src/main/kotlin/com/tboul/sudoku/views/GridView.kt
@@ -6,6 +6,7 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.graphics.Paint.ANTI_ALIAS_FLAG
+import android.graphics.RectF
 import android.util.AttributeSet
 import android.view.View
 import com.tboul.sudoku.models.Grid
@@ -16,21 +17,34 @@ class GridView(private val grid: Grid, context: Context?, attrs: AttributeSet? =
     View(context, attrs) {
     private val paint = Paint(ANTI_ALIAS_FLAG)
 
-    private var gridSeparatorSize = 0.toFloat()
-    private var gridWidth = 0.toFloat()
-    private var cellWidth = 0.toFloat()
+    private var gridSeparatorSize = 0F
+    private var gridWidth = 0F
+    private var cellWidth = 0F
+    private var buttonWidth = 0F
+    private var buttonRadius = 0F
+    private var buttonMargin = 0F
+
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
 
+        // Separator size
         gridSeparatorSize = (w / 9F) / 20F
+
+        // Size of cells
         gridWidth = w.toFloat()
         cellWidth = gridWidth / 9F
+
+        // Size of buttons
+        buttonWidth = w / 7F
+        buttonRadius = buttonWidth / 10F
+        buttonMargin = (w - 6 * buttonWidth) / 7F
     }
 
     override fun onDraw(canvas: Canvas?) {
         paint.textAlign = Paint.Align.CENTER
 
+        // Draw the sudoku
         for (i in 0 until 8) {
             for (j in 0 until 8) {
                 val backgroundColor = if (!grid[i][j].visible) Color.WHITE else 0xFFF0F0F0.toInt()
@@ -105,6 +119,7 @@ class GridView(private val grid: Grid, context: Context?, attrs: AttributeSet? =
             }
         }
 
+        // Draw lines grid
         paint.color = Color.GRAY
         paint.strokeWidth = gridSeparatorSize / 2
 
@@ -118,6 +133,43 @@ class GridView(private val grid: Grid, context: Context?, attrs: AttributeSet? =
         for (i in 0..3) {
             canvas?.drawLine(i * (cellWidth * 3), 0F, i * (cellWidth * 3), cellWidth * 9, paint)
             canvas?.drawLine(0F, i * (cellWidth * 3), cellWidth * 9, i * (cellWidth * 3), paint)
+        }
+
+        // Draw buttons number
+        val buttonZone = 9 * cellWidth + gridSeparatorSize / 2
+
+        paint.textAlign = Paint.Align.CENTER
+        paint.textSize = buttonWidth * 0.7F
+
+        var buttonLeft = buttonMargin
+        var buttonTop = buttonZone + buttonMargin
+
+        @SuppressLint("DrawAllocation")
+        for (i in 1..9) {
+            paint.color = 0xFFFFFFFFF.toInt()
+            paint.style = Paint.Style.FILL
+            val rectF =
+                RectF(buttonLeft, buttonTop, buttonLeft + buttonWidth, buttonTop + buttonWidth)
+            canvas?.drawRoundRect(rectF, buttonRadius, buttonRadius, paint)
+
+            paint.color = 0xFF000000.toInt()
+            canvas?.drawText(
+                i.toString(),
+                rectF.centerX(),
+                rectF.top + rectF.height() * 0.75f,
+                paint
+            )
+
+            paint.color = Color.BLACK
+            paint.style = Paint.Style.STROKE
+            canvas?.drawRoundRect(rectF, buttonRadius, buttonRadius, paint)
+
+            if (i != 6) {
+                buttonLeft += buttonWidth + buttonMargin
+            } else {
+                buttonLeft = buttonMargin;
+                buttonTop += buttonWidth + buttonMargin
+            }
         }
     }
 }
