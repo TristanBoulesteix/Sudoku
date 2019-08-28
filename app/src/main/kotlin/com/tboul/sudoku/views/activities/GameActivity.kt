d9b67f33f8010b414ba9706d83944a13b67b6a38
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-08-28 20:17:23 +0200

    Add setting activity

--- app/src/main/kotlin/com/tboul/sudoku/views/activities/GameActivity.kt
+++ app/src/main/kotlin/com/tboul/sudoku/views/activities/GameActivity.kt
@@ -1,5 +1,7 @@
 package com.tboul.sudoku.views.activities
 
+import android.app.ActivityOptions
+import android.content.Intent
 import android.os.Bundle
 import android.view.Window
 import android.widget.FrameLayout
@@ -19,6 +21,15 @@ class GameActivity : AppCompatActivity() {
         setContentView(R.layout.activity_game)
 
         findViewById<FloatingActionButton>(R.id.fab_reset).setOnClickListener(gridView.clickReset)
+        findViewById<FloatingActionButton>(R.id.fab_settings).setOnClickListener {
+            val gameActivity = Intent(this@GameActivity, SettingsActivity::class.java)
+            val transition = ActivityOptions.makeCustomAnimation(
+                this@GameActivity,
+                R.anim.transition_start,
+                R.anim.trantion_end
+            ).toBundle()
+            startActivity(gameActivity, transition)
+        }
         findViewById<FrameLayout>(R.id.sudoku).addView(gridView)
     }
 }
