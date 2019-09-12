990845c97782de0a0d3ef88376c7c09bf4a91125
TristanBoulesteix <tristan.boulesteix@gmail.com>
2019-09-12 19:24:10 +0200

    Google play sign in can get user data

--- app/src/main/kotlin/com/tboul/sudoku/views/activities/templates/TemplateActivity.kt
+++ app/src/main/kotlin/com/tboul/sudoku/views/activities/templates/TemplateActivity.kt
@@ -1,13 +1,90 @@
 package com.tboul.sudoku.views.activities.templates
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Window
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.Auth
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 
 abstract class TemplateActivity : AppCompatActivity() {
+    private val signInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_GAMES_SIGN_IN).requestProfile().build()
+
+    private var signedInAccount: GoogleSignInAccount? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         supportActionBar?.hide()
+    }
+
+    private fun signInSilently() {
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (GoogleSignIn.hasPermissions(account, *signInOptions.scopeArray)) {
+            // Already signed in.
+            // The signed in account is stored in the 'account' variable.
+            signedInAccount = account
+            signedInAccount.let {
+                Toast.makeText(
+                    this,
+                    signedInAccount!!.displayName,
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else {
+            // Haven't been signed-in before. Try the silent sign-in first.
+            val signInClient = GoogleSignIn.getClient(this, signInOptions)
+            signInClient
+                .silentSignIn()
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // The signed in account is stored in the task's result.
+                        val signedInAccount = task.result
+                    } else {
+                        // Player will need to sign-in explicitly using via UI.
+                        // See [sign-in best practices](http://developers.google.com/games/services/checklist) for guidance on how and when to implement Interactive Sign-in,
+                        // and [Performing Interactive Sign-in](http://developers.google.com/games/services/android/signin#performing_interactive_sign-in) for details on how to implement
+                        // Interactive Sign-in.
+                        startSignIn()
+                    }
+                }
+        }
+    }
+
+    private fun startSignIn() {
+        val signInClient = GoogleSignIn.getClient(
+            this,
+            signInOptions
+        )
+        val intent = signInClient.signInIntent
+        startActivityForResult(intent, 2)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 2) {
+            val result = Auth.GoogleSignInApi.getSignInResultFromIntent(data)
+            if (result.isSuccess) {
+                // The signed in account is stored in the result.
+                signedInAccount = result.signInAccount
+            } else {
+                var message = result.status.statusMessage
+                if (message == null || message.isEmpty()) {
+                    message = "FAIL"
+                }
+                /* AlertDialog.Builder(this).setMessage(message)
+                     .setNeutralButton(android.R.string.ok, null).show()*/
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        signInSilently()
+        if(signedInAccount != null) Toast.makeText(this, signedInAccount!!.email, Toast.LENGTH_SHORT).show()
     }
 }
